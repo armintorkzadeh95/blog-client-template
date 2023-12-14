0cb3eefaaa6299a1cf48c8b3c885b14c528fd963
Armin Torkzadeh <armin.torkzadeh@outlook.com>
2023-12-14 15:13:07 +0100

    change code to proper places in post.js

--- js/post.js
+++ js/post.js
@@ -1,49 +1,38 @@
-// Hämta frågesträngen från URL:en
-const queryString = window.location.search;
-
-// Analysera frågesträngen för att få ett objekt med parameter-värdepar
-const params = new URLSearchParams(queryString);
-
-// Hämta värdet som är kopplat till parametern "id".
-const postId = params.get('id');
-
-// Logga postId till konsolen
-console.log("Post ID:", postId);
-
-// Förutsatt att du har en API-slutpunkt som denna
+// API-slutpunkt för blogginlägg
 const apiUrl = "https://blog-api-assignment.up.railway.app/posts";
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Använd postId-variabeln i funktionen fetchPostDetails
-    fetchPostDetails(postId);
-});
-
+// Funktion för att hämta inläggsdetaljer med postId
 function fetchPostDetails(postIndex) {
-    // Gör GET-förfrågan till API-slutpunkten
     fetch(`${apiUrl}/${postIndex}`)
         .then(response => response.json())
         .then(post => displayPostDetails(post))
         .catch(error => console.error('Error fetching post:', error));
 }
 
+// Funktion för att visa inläggsdetaljer på sidan
 function displayPostDetails(post) {
     document.getElementById("postTitle").innerText = post.title;
     document.getElementById("postAuthor").innerText = "Author: " + post.author;
     document.getElementById("postDate").innerText = "Date: " + post.date;
-
-    // Använd en ternär operator för att kontrollera om post.tags är definierade
     document.getElementById("postTags").innerText = "Tags: " + (post.tags ? post.tags.join(", ") : "N/A");
-
     document.getElementById("postContent").innerHTML = post.content;
 }
 
+// Funktion för att gå tillbaka med webbläsarhistorik
 function goBack() {
-    // Använd webbläsarens historikobjekt för att navigera tillbaka
     window.history.back();
 }
 
-// funktion för att visa mer innehåll
+// Händelseavlyssnare för DOMContentLoaded för att hämta inläggsdetaljer
+document.addEventListener("DOMContentLoaded", function () {
+    const queryString = window.location.search;
+    const params = new URLSearchParams(queryString);
+    const postId = params.get('id');
+    console.log("Post ID:", postId);
+    fetchPostDetails(postId);
+});
+
+// Funktion för att visa mer innehåll
 window.readMore = function(content) {
-   // Istället för att varna visa innehållet i en modal eller uppdatera sidan efter behov
     document.getElementById("postContent").innerHTML = content;
 };
