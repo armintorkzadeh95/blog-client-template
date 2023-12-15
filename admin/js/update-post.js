ee54a9c04bf7a5a2f54cc1e5daba5c6da786de8c
Armin Torkzadeh <armin.torkzadeh@outlook.com>
2023-12-15 16:12:07 +0100

    comment done

--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -1,17 +1,21 @@
+// Den här funktionen fyller formuläret med data som erhållits från funktionen getPostData().
 function fillForm() {
     const postData = getPostData();
+
+    // Ställ in värdet på inmatningsfältet 'titel' till titeln och textområdet "innehåll" till innehållet från postData
     document.getElementById('title').value = postData.title;
     document.getElementById('content').value = postData.content;
 }
+// Funktionen uppdaterar ett inlägg med data som anges i formuläret.
 function updatePost() {
     const updatedData = {
         title: document.getElementById('title').value,
         content: document.getElementById('content').value
     };
-
+    // Anrop funktionen sendUpdatedPost() med den uppdaterade datan
     if (sendUpdatedPost(updatedData)) {
         window.location.href = 'admin/index.html';
     }
 }
-
+// Anrop funktionen fillForm() för att fylla i formuläret med initiala data
 fillForm();
