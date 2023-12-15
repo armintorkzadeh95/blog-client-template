ccebf7213ed4b0d2fe5fe7149ec5be89a3bec841
Armin Torkzadeh <armin.torkzadeh@outlook.com>
2023-12-15 14:05:02 +0100

    function change in post.js

--- js/post.js
+++ js/post.js
@@ -2,3 +2,16 @@ const urlParams = new URLSearchParams(window.location.search);
 const postId = urlParams.get('id');
 
 fetchPostDetails(postId);
+
+async function fetchPostDetails(id) {
+    try {
+        const response = await fetch(`https://blog-api-assignment.up.railway.app/posts/${id}`);
+        const post = await response.json();
+        console.log(post)
+
+        document.getElementById('post-content').innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.author} ${new Date(post.date).toLocaleDateString()}</p>
+            <p>${post.tags}</p>
+            <p>${post.content}</p>
+        `;
